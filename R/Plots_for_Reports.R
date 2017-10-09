e3adea7ba17f6d9cdc3e2a7a3c08fda27435813f
Amar <amarsarvepalli19@gmail.com>
2017-10-09 12:43:46 -0400

    plot for reports: Diverted EL Shares function
    adds function to produce EL share of corridor by hour & direction

--- R/Plots_for_Reports.R
+++ R/Plots_for_Reports.R
@@ -14,7 +14,6 @@ df %>% filter(Policy == "newPolicy", Year == 2040, Seg == 1) %>%
    plot_ly(x = ~Hour, y = ~EL_NB_SHARE, mode = 'lines', type = "scatter", name = "EL NB") %>%
    add_trace(y = ~EL_SB_SHARE, mode = 'lines', type = "scatter", name = "EL SB")
 
-
 # Using ggplot
   df %>% filter(Policy == "newPolicy", Year == 2040) %>%
    select("EL_NB_SHARE", "EL_SB_SHARE", "Hour", "Seg") %>%
@@ -24,28 +23,40 @@ df %>% filter(Policy == "newPolicy", Year == 2040, Seg == 1) %>%
    facet_wrap(Seg ~ Direction, labeller = label_both) +
    theme_linedraw()
 
-# Using plotly 
-data <- df %>% filter(Policy == "newPolicy", Year == 2040, Seg == 1)
+# function to plot diverted shares
+plot_diverted_shares <- function(data, plot_title) {
+  # Axes settings
+  x_labels <- list(title = "Hour", 
+                   color = "grey", 
+                   titlefont = list(color = "black", size = 20, 
+                                    family = list("Arial", "Time New Roman")),
+                   autotick = FALSE, ticks = "outside", 
+                   tick0 = 0, dtick = 1, range = c(0.25,24.5))
+  y_labels <- list(title = "EL Share (%)",color = "grey", 
+                   titlefont = list(color = "black", size = 20, 
+                                    family = list("Arial", "Time New Roman")),
+                   autotick = FALSE, ticks = "outside", 
+                   tick0 = 0, dtick = .05, tickformat = ".0%")
+  data %>%
+     plot_ly(x = ~Hour, y = ~EL_NB_SHARE, mode = 'lines+markers', type = "scatter", name = "EL NB") %>%
+     add_trace(y = ~EL_SB_SHARE, mode = 'lines+markers', type = "scatter", name = "EL SB") %>%
+     layout(title = plot_title, # "Diverted Percent (EL Share)",
+            titlefont = list(color = "black", size = 25, 
+                                    family = list("Arial", "Time New Roman")),
+            xaxis = x_labels, 
+            yaxis = y_labels,
+            margin = list(t = 100, b = 100, l = 100, pad = 5))
+}
 
-# Axes settings
-x_labels <- list(title = "Hour", 
-                 color = "grey", 
-                 titlefont = list(color = "black", size = 20, 
-                                  family = list("Arial", "Time New Roman")),
-                 autotick = FALSE, ticks = "outside", 
-                 tick0 = 0, dtick = 1, range = c(0.25,24.5))
-y_labels <- list(title = "EL Share (%)",color = "grey", 
-                 titlefont = list(color = "black", size = 20, 
-                                  family = list("Arial", "Time New Roman")),
-                 autotick = FALSE, ticks = "outside", 
-                 tick0 = 0, dtick = .05, tickformat = ".0%")
-data %>%
-   plot_ly(x = ~Hour, y = ~EL_NB_SHARE, mode = 'lines+markers', type = "scatter", name = "EL NB") %>%
-   add_trace(y = ~EL_SB_SHARE, mode = 'lines+markers', type = "scatter", name = "EL SB") %>%
-   layout(title = "Diverted Percent (EL Share)",
-          titlefont = list(color = "black", size = 25, 
-                                  family = list("Arial", "Time New Roman")),
-          xaxis = x_labels, 
-          yaxis = y_labels,
-          margin = list(t = 100, b = 100, l = 100, pad = 5))
+# Using plotly 
+diverted_el_shares <- df %>% filter(Policy == "newPolicy", Year == 2040, Seg == 1) %>%
+       plot_diverted_shares("Diverted Percent (EL Share)")
+
+# save it as pdf 
+# export(diverted_el_shares, paste0(dir_path,"/plot.png"))
+ 
+
+
+
+
   
